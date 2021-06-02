7270b28e7f57c52b741b9689a301b9287ef8bdbb
Kartikey Mahawar <kartikeymahawar1234@gmail.com>
2021-06-02 12:54:09 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+import Home from './screens/home';
+import { View } from 'react-native';
+import Navigator from './routes/homestack';
+
+
+
+export default function App() {
+  return (
+    <Navigator />
+  );
+}
---
+++ styles/global.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24
+    },
+    titletext: {
+        fontSize: 18,
+        fontFamily: 'Pacifico-Regular',
+        color: '#333'
+
+    },
+    paragraph: {
+        marginVertical: 8,
+        lineHeight: 20,
+    }
+});
